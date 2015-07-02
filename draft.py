9f609b0e3f8074b8bb9ca70a93719a2aff42d2c7
yifaan <yfyang0814@gmail.com>
2015-07-02 15:06:17 -0400

    some format change

--- draft.py
+++ draft.py
@@ -16,17 +16,17 @@ class controller():
         self.c.populate(len(modules), modules)
         self.pos_1 = self.c.at.MOD1.get_pos()
         self.pos_2 = self.c.at.MOD2.get_pos()
-	self.pos_3 = self.c.at.MOD3.get_pos()
+        self.pos_3 = self.c.at.MOD3.get_pos()
         # position of the three motor
         self.x1 = 0
         self.y1 = 200
         self.z1 = 0
         self.x2 = -173
         self.y2 = -100
-	self.z2 = 0
+        self.z2 = 0
         self.x3 = 173
         self.y3 = -100
-	self.z3 = 0
+        self.z3 = 0
         self.range = 5  # about 0.6 degree
         print "initialize done"
 
@@ -34,7 +34,7 @@ class controller():
     def get_pos(self):
         self.pos_1 = self.c.at.MOD1.get_pos()
         self.pos_2 = self.c.at.MOD2.get_pos()
-	self.pos_3 = self.c.at.MOD3.get_pos()
+        self.pos_3 = self.c.at.MOD3.get_pos()
 
     # move the motor according to the speed
     def moveMOD1(self, speed):
@@ -89,13 +89,13 @@ class controller():
 
     # the inverse kinematics of the arm
     def pos2len(self, x, y, z):
-        #(x1=,y1=,z1=), (x2=,y2=,z2=)(x3=,y3=,z3=) asda
+        # (x1=,y1=,z1=), (x2=,y2=,z2=)(x3=,y3=,z3=)
         len1 = ((x - self.x1) ** 2 + (y - self.y1) ** 2 + (z - self.z1) ** 2) ** 0.5
         len2 = ((x - self.x2) ** 2 + (y - self.y2) ** 2 + (z - self.z2) ** 2) ** 0.5
         len3 = ((x - self.x3) ** 2 + (y - self.y3) ** 2 + (z - self.z3) ** 2) ** 0.5
         return (len1, len2, len3)
 
-    # forward kinematics of the 
+    # forward kinematics of the
     def len2pos(self, l1, l2, l3):
         # mat1 * [x,y]' = mat2
         mat1 = [[2*(self.x3 - self.x1),2*(self.y3 - self.y1)],[2*(self.x2 - self.x1),2*(self.x3 - self.y1)]]
@@ -104,16 +104,16 @@ class controller():
         inv_mat1 = [[mat1[1][1],-mat1[0][1]],[-mat1[1][0],mat1[0][0]]]
         x = (mat2[0] * inv_mat1[0][0] + mat2[1] * inv_mat1[0][1]) / det1
         y = (mat2[0] * inv_mat1[1][0] + mat2[1] * inv_mat1[1][1]) / det1
-        z = (l1**2 - (x - self.x1)**2  - (y - self.y1)**2) ** 0.5
+        z = (l1**2 - (x - self.x1)**2 - (y - self.y1)**2) ** 0.5
         return (x, y, z)
 
     # change endeffector speed to motor command value
     def end2servo(self, x, y, z, Vx, Vy, Vz):
-        # get the norm of the vector of each cable 
+        # get the norm of the vector of each cable
         l1_norm = ((x - self.x1)**2 + (y - self.y1)**2 + (z - self.z1)**2)**0.5
         l2_norm = ((x - self.x2)**2 + (y - self.y2)**2 + (z - self.z2)**2)**0.5
         l3_norm = ((x - self.x3)**2 + (y - self.y3)**2 + (z - self.z3)**2)**0.5
-	
+
         # get the unit vector on each cable's direction
         u1 = [(x - self.x1) / l1_norm, (y - self.y1) / l1_norm, (z - self.z1) / l1_norm]
         u2 = [(x - self.x2) / l2_norm, (y - self.y2) / l2_norm, (z - self.z2) / l2_norm]
@@ -122,127 +122,127 @@ class controller():
         V1 = Vx * u1[0] + Vy * u1[1] + Vz * u1[2]
         V2 = Vx * u2[0] + Vy * u2[1] + Vz * u2[2]
         V3 = Vx * u3[0] + Vy * u3[1] + Vz * u3[2]
-	
+
         # calculate the commanded torque value according to the cable speed
-	# rpm = 80 * set_torque(value) - 1
-    	# deg/sec = 480 * set_torque(value) - 6
+        # rpm = 80 * set_torque(value) - 1
+        # deg/sec = 480 * set_torque(value) - 6
         # 120 mm per rotation, 120/360 = 1/3 mm/deg
-	# mm/sec = 160 * set_torque(value) - 2
-	if (V1>0):
-	    tval1 = (V1 + 2.0) / 160
-	else:
-	    tval1 = (V1 - 2.0) / 160
-	if (V2>0):
-	    tval2 = (V2 + 2.0) / 160
-	else:
-	    tval2 = (V2 - 2.0) / 160
-	if (V3>0):
-	    tval3 = (V3 + 2.0) / 160
-	else:
-	    tval3 = (V3 - 2.0) / 160
-        return (tval1, tval2, tval3)
-    
-    # move in z-direction 
+        # mm/sec = 160 * set_torque(value) - 2
+        if (V1 > 0):
+            tval1 = (V1 + 2.0) / 160
+        else:
+            tval1 = (V1 - 2.0) / 160
+        if (V2 > 0):
+            tval2 = (V2 + 2.0) / 160
+        else:
+            tval2 = (V2 - 2.0) / 160
+        if (V3 > 0):
+            tval3 = (V3 + 2.0) / 160
+        else:
+            tval3 = (V3 - 2.0) / 160
+            return (tval1, tval2, tval3)
+
+    # move in z-direction
     def traj1(self, t):
-	if (t<5):
-	    x = 0
-	    y = 0
-	    z = 20*t
-	    Vx = 0
-	    Vy = 0
-	    Vz = 20
-        elif ((t>=5) and (t<10)):
-	    x = 0
-	    y = 0
-	    z = 100 - 20*(t-5)
-	    Vx = 0
-	    Vy = 0
-	    Vz = -20
-	else:
-	    x = 0
-	    y = 0            
-	    z = 0
-	    Vx = 0
-	    Vy = 0
-	    Vz = 0
-	return (x, y, z, Vx, Vy, Vz)
+        if (t < 5):
+            x = 0
+            y = 0
+            z = 20*t
+            Vx = 0
+            Vy = 0
+            Vz = 20
+        elif ((t >= 5) and (t < 10)):
+            x = 0
+            y = 0
+            z = 100 - 20*(t-5)
+            Vx = 0
+            Vy = 0
+            Vz = -20
+        else:
+            x = 0
+            y = 0
+            z = 0
+            Vx = 0
+            Vy = 0
+            Vz = 0
+        return (x, y, z, Vx, Vy, Vz)
 
-    # move in x-direction 
+    # move in x-direction
     def traj2(self, t):
-	if (t<5):
-	    x = 20*t
-	    y = 0
-	    z = 0
-	    Vx = 20
-	    Vy = 0
-	    Vz = 0
-        elif (t<10):
-	    x = 100 - 20*(t-5)
-	    y = 0
-	    z = 0
-	    Vx = -20
-	    Vy = 0
-	    Vz = 0
-	else:
-	    x = 0
-	    y = 0            
-	    z = 0
-	    Vx = 0
-	    Vy = 0
-	    Vz = 0
-	return (x, y, z, Vx, Vy, Vz)
+        if (t < 5):
+            x = 20*t
+            y = 0
+            z = 0
+            Vx = 20
+            Vy = 0
+            Vz = 0
+        elif (t < 10):
+            x = 100 - 20 * (t - 5)
+            y = 0
+            z = 0
+            Vx = -20
+            Vy = 0
+            Vz = 0
+        else:
+            x = 0
+            y = 0
+            z = 0
+            Vx = 0
+            Vy = 0
+            Vz = 0
+        return (x, y, z, Vx, Vy, Vz)
 
-    # move in y-direction 
+    # move in y-direction
     def traj3(self, t):
-	x = 0
-	y = 10 * t
-	z = 0
-	Vx = 0
-	Vy = 10
-	Vz = 0
-	if (t>10):
-	    y = 10 * 10
-	    Vy = 0
-	return (x, y, z, Vx, Vy, Vz)
+        x = 0
+        y = 10 * t
+        z = 0
+        Vx = 0
+        Vy = 10
+        Vz = 0
+        if (t > 10):
+            y = 10 * 10
+            Vy = 0
+        return (x, y, z, Vx, Vy, Vz)
 
-    # move in xyz-direction 
+    # move in xyz-direction
     def traj4(self, t):
-	if (t<5):
-	   x = 10 * t
-	   y = 10 * t
-	   z = 20 * t
-	   Vx = 10
-	   Vy = 10
-	   Vz = 20
-	elif ((t<10) and (t>=5)):
-	   x = 50-10 * (t-5)
-	   y = 50-10 * (t-5)
-	   z = 100+20 * (t-5)
-	   Vx = -10
-	   Vy = -10
-	   Vz = 20
-	elif ((t<15) and (t>=10)):
-	   x = -10 * (t-10)
-	   y = -10 * (t-10)
-	   z = 200-20 * (t-10)
-	   Vx = -10
-	   Vy = -10
-	   Vz = -20
-	elif ((t<20) and (t>=15)):
-	   x = -50+10 * (t-15)
-	   y = -50+10 * (t-15)
-	   z = 100-20 * (t-15)
-	   Vx = 10
-	   Vy = 10
-	   Vz = -20
-	else:
-	    x = 0
-	    Vx = 0
- 	    y = 0
-	    Vy = 0
-	    z = 0
-	    Vz = 0
-	return (x, y, z, Vx, Vy, Vz)
+        if (t < 5):
+            x = 10 * t
+            y = 10 * t
+            z = 20 * t
+            Vx = 10
+            Vy = 10
+            Vz = 20
+        elif ((t < 10) and (t >= 5)):
+            x = 50-10 * (t-5)
+            y = 50-10 * (t-5)
+            z = 100+20 * (t-5)
+            Vx = -10
+            Vy = -10
+            Vz = 20
+        elif ((t < 15) and (t >= 10)):
+            x = -10 * (t-10)
+            y = -10 * (t-10)
+            z = 200-20 * (t-10)
+            Vx = -10
+            Vy = -10
+            Vz = -20
+        elif ((t < 20) and (t >= 15)):
+            x = -50+10 * (t-15)
+            y = -50+10 * (t-15)
+            z = 100-20 * (t-15)
+            Vx = 10
+            Vy = 10
+            Vz = -20
+        else:
+            x = 0
+            Vx = 0
+            y = 0
+            Vy = 0
+            z = 0
+            Vz = 0
+        return (x, y, z, Vx, Vy, Vz)
 
     # stop motor and go_slack on each module
     def shut_down(self):
@@ -257,14 +257,6 @@ class controller():
 
 if __name__ == "__main__":
     c1 = controller()
-
-    #pos = c1.len2pos(200, 300, 200)
-    
-    #print pos[0]
-    #print pos[1]
-    #print pos[2]
-    
-
     c1.move_to(2500, 2500, 2500, 0.08)
     # c1.move_to(10000, 10000, 10000, 0.08)
     # c1.move_to(3000, 3000, 3000, 0.08)
@@ -278,22 +270,18 @@ if __name__ == "__main__":
     while 1:
         try:
             traj = c1.traj4(time() - t0)
-	    servo_speed = c1.end2servo(traj[0],traj[1],traj[2],traj[3],traj[4],traj[5])
+            servo_speed = c1.end2servo(traj[0], traj[1], traj[2], traj[3], traj[4], traj[5])
+            # sys.stdout.write("%.2f\r" % (time() - t0))
+            # sys.stdout.write("%.3f\r" % servo_speed[0])
+            # sys.stdout.write("%5d\n" % c1.pos_1)
+            # sys.stdout.flush()
 
-            #sys.stdout.write("%.2f\r" % (time() - t0))
-            #sys.stdout.write("%.3f\r" % servo_speed[0])
-            #sys.stdout.write("%5d\n" % c1.pos_1)
-             
-            #sys.stdout.flush()
-
-		
             c1.moveMOD1(servo_speed[0])
             c1.moveMOD2(servo_speed[1])
-	    c1.moveMOD3(servo_speed[2])
-
+            c1.moveMOD3(servo_speed[2])
 
         except KeyboardInterrupt:
-	    break
+            break
 
     c1.move_to(2500, 2500, 2500, 0.08)
     c1.shut_down()
